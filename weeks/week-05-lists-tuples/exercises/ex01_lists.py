f595addfa2a0b323b3e605aaf2c919da7428f969
Bui Trung Son <buitrungson09@gmail.com>
2026-09-23 14:33:57 +0700

    feat: nop bai tap week 05

--- weeks/week-05-lists-tuples/exercises/ex01_lists.py
+++ weeks/week-05-lists-tuples/exercises/ex01_lists.py
@@ -3,8 +3,19 @@
 subjects = ["Toán", "Văn", "Anh"]
 
 # TODO: append one subject and insert another at index 1.
-# TODO: update the first subject.
-# TODO: remove one known subject and pop the last subject.
-# TODO: print the first, last and middle slice after each safe operation.
 
-print(subjects)
+subjects.append("Tin học")      # Thêm vào cuối
+subjects.insert(1, "Vật lý")    # Chèn vào vị trí 1
+print("Sau khi thêm:", subjects)
+print("Đầu:", subjects[0], "| Cuối:", subjects[-1], "| Giữa:", subjects[1:-1])
+
+# TODO: update the first subject.
+subjects[0] = "Hóa học"         # Sửa phần tử đầu
+print("Sau khi sửa:", subjects)
+print("Đầu:", subjects[0], "| Cuối:", subjects[-1], "| Giữa:", subjects[1:-1])
+# TODO: remove one known subject and pop the last subject.
+subjects.remove("Văn")         # Xóa theo giá trị
+subjects.pop()                 # Xóa phần tử cuối
+print("Sau khi xóa:", subjects)
+print("Đầu:", subjects[0], "| Cuối:", subjects[-1], "| Giữa:", subjects[1:-1])
+
--- weeks/week-05-lists-tuples/exercises/ex02_slicing.py
+++ weeks/week-05-lists-tuples/exercises/ex02_slicing.py
@@ -3,12 +3,18 @@
 numbers = [1, 2, 3, 4, 5, 6]
 
 # TODO: create first_three and last_three using slices.
-first_three: list[int] = []
-last_three: list[int] = []
+first_three: list[int] = numbers[:3]
+last_three: list[int] = numbers[-3:]
 
 # TODO: make alias refer to numbers and copied be a shallow copy.
-alias: list[int] = []
-copied: list[int] = []
+alias: list[int] = numbers
+copied: list[int] = numbers.copy()
+
+alias.append(7)
 
 # TODO: append through alias and explain which lists change.
-print(first_three, last_three, alias, copied)
+print("Ba số đầu:", first_three)
+print("Ba số cuối:", last_three)
+print("numbers:", numbers)
+print("alias:", alias)
+print("copied:", copied)
