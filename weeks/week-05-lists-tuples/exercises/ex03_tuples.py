da0c39ee17ac992065c88e0d4834c939996638cf
Bui Trung Son <buitrungson09@gmail.com>
2026-09-23 13:33:35 +0700

    Hoan thanh bai tap Week 05

--- weeks/week-05-lists-tuples/exercises/ex03_tuples.py
+++ weeks/week-05-lists-tuples/exercises/ex03_tuples.py
@@ -5,12 +5,21 @@ coordinate = (3, 7)
 # TODO: unpack coordinate into x and y.
 x = 0
 y = 0
+x, y = coordinate
 
 # TODO: pack name, age and topic into one profile tuple, then unpack it.
 profile: tuple[str, int, str] = ("", 0, "")
-
+name = "Sơn"
+age = 19
+topic = "Python"
+profile: tuple[str, int, str] = (name, age, topic)
+profile_name, profile_age, profile_topic = profile
 # TODO: swap left and right using unpacking.
 left = "A"
 right = "B"
+left, right = right, left
 
-print(x, y, profile, left, right)
+print("Tọa độ:", x, y)
+print("Hồ sơ:", profile)
+print("Tách hồ sơ:", profile_name, profile_age, profile_topic)
+print("Sau khi đổi chỗ:", left, right)
